54ec10e729d74f1f06f30f65d794eab1765da90a
unknown <kesra476@gmail.com>
2025-05-02 09:26:59 +0300

    Reverted to version before feature-v2 merge

--- OOP/3x+1.php
+++ OOP/3x+1.php
@@ -40,7 +40,7 @@
 
         if($x>1){
             echo "<h3>Starting 3x+1 algorithm, the number is <span style='color: orangered;'>$x</span> <br/></h3>";
-            echo "<h3 style='color: orangered;'>-------------------------------------------------------------</h3>";
+            echo "";
             while($x>1){
                 if($x%2===0){
                     $x/=2;
@@ -51,7 +51,6 @@
                 $y++;
                 echo "<span style='color: orangered;'>Step $y :</span> Number is $x <br/>";
             }
-            echo "<h3 style='color: orangered;'>-------------------------------------------------------------</h3>";
             echo "<h3>Algorithm finished in <span style='color: orangered;'>$y</span> steps.</h3> <br/>";
         }else{
             echo "<h3>The number must be greater than 1.</h3><br/>";
